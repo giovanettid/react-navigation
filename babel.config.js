f439872adbb5449c851c04ad476425d53d971ec0
Didier Giovanetti <didier.giovanetti@gmail.com>
2023-03-28 17:38:20 +0200

    leaflet: setup react-leaflet

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    '@babel/env',
+    ['@babel/react', { runtime: 'automatic' }],
+  ],
+  plugins: [
+    ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
+  ],
+};
--- src/components/Configuration.js
+++ src/components/Configuration.js
@@ -1,5 +1,8 @@
 export default class Configuration {
   constructor() {
-    this.name = 'starter';
+    this.center = [48.856, 2.352];
+    this.initialZoom = 13;
+    this.urlTilesTemplate = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    this.attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
   }
 }
